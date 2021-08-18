bb8848855317d8728b63aa3541b345d6040e2a02
Erick Murillo <erickmurillo22@gmail.com>
2021-08-18 10:45:26 -0600

    subiendo template

--- consultorio_velez_v2/settings.py
+++ consultorio_velez_v2/settings.py
@@ -10,24 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
-import os
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+from .local_settings import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-2pex3b8ad%$_@zclpznmv-%pb#@-tz3o*60f=_+6%a#p939vdu'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -38,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'resumenes_clinicos',
 ]
 
 MIDDLEWARE = [
@@ -69,17 +59,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'consultorio_velez_v2.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
